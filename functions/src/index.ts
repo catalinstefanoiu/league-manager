8b6554b131de14f0980760f94f47bf218c01c24b
Razvan <razvan.stefanoiu@vxmt.com>
2024-12-24 12:29:47 +0200

    set initial admin working

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,12 @@
 /**
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ *
+ * https://firebase.google.com/docs/functions/typescript
  */
 
+import { initializeApp } from 'firebase-admin/app';
 import { logger } from 'firebase-functions';
-import { auth } from 'firebase-functions/v1';
+import * as functions from 'firebase-functions/v1';
 import { setGlobalOptions } from 'firebase-functions/v2';
 import { onRequest } from 'firebase-functions/v2/https';
 import { UserRole } from './user';
@@ -13,32 +16,34 @@ setGlobalOptions({
   region: 'europe-central2'
 });
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+initializeApp();
+
 
 export const helloWorld = onRequest({
   region: 'europe-central2'
-},(request, response) => {
+}, (request, response) => {
   logger.info('Hello logs!', { structuredData: true });
   response.send('Hello from Firebase!');
 });
 
-export const setInitialAdmin = auth.user().onCreate(async (user) => {
-  const admins = ['dev.stefanoiu@gmail.com'];
-  if (
-    user.email &&
-    user.emailVerified &&
-    admins.includes(user.email)
-  ) {
-    const customClaims = {
-      role: UserRole.AppAdmin
-    };
+export const setInitialAdmin = functions
+  .region('europe-central2')
+  .auth.user().onCreate(async (user) => {
+    const admins = ['dev.stefanoiu@gmail.com'];
+    if (
+      user.email &&
+      user.emailVerified &&
+      admins.includes(user.email)
+    ) {
+      const customClaims = {
+        role: UserRole.AppAdmin
+      };
 
-    try {
-      await getAuth().setCustomUserClaims(user.uid, customClaims);
-      logger.info(`custom claims set on ${user.email}`);
-    } catch (ex) {
-      logger.error(ex, { structuredData: true });
+      try {
+        await getAuth().setCustomUserClaims(user.uid, customClaims);
+        logger.info(`custom claims set on ${user.email}`);
+      } catch (ex) {
+        logger.error(ex, { structuredData: true });
+      }
     }
-  }
-});
+  });
